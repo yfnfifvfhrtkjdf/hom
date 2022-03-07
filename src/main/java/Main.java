9b97e3aaedb923861caee20eadb26934fc4df6e8
123 <rotomop11@yandex.ru>
2022-03-08 01:19:15 +0300

    3 dz

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,21 @@
 import model.Calculator;
+import model.CalculatorException;
 
 import java.util.Scanner;
 
-public class Main extends  Calculator {
+public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Первое число: ");
-        String line1=in.nextLine();
+        System.out.println("Арифметическая операция +, -, *: ");
+        String operation=in.nextLine();
 
-        System.out.println("Арифметическая операция: ");
-        String arifmeticOperation=in.nextLine();
+        System.out.println("Первое число: ");
+        String line=in.nextLine();
 
         System.out.println("Второе число: ");
-        String line2=in.nextLine();
-        System.out.println("Результат операции: "+ Calculator.execute(new String[]{line1, arifmeticOperation, line2} ));
+        String line1=in.nextLine();
+
+
+        System.out.println("Результат операции: "+ Calculator.execute(new String[]{line,operation, line}));
     }
 }
--- src/main/java/model/Calculator.java
+++ src/main/java/model/Calculator.java
@@ -1,6 +1,7 @@
 package model;
 
 public class Calculator {
+
     public static String execute(String[] params)  {
     int a = Integer.parseInt(params[0]);
         int b = Integer.parseInt(params[2]);
