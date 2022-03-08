7d8f6cbaf0bf3eb5258dc22e6e954af42f219da1
123 <rotomop11@yandex.ru>
2022-03-08 04:56:31 +0300

    3 dz

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,15 +8,15 @@ public class Main{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String operation=in.nextLine();
-        System.out.println(operation);
+       // System.out.println(operation);
 
         String line=in.nextLine();
-        System.out.println(line);
+      //  System.out.println(line);
 
         String line1=in.nextLine();
-        System.out.println(line1);
-
-        System.out.println("Результат операции: "+ Calculator.execute(new String[]{line,operation, line1}));
+       // System.out.println(line1);
+        Calculator.execute(new String[]{line,operation, line1});
+       // System.out.println("Результат операции: "+ Calculator.execute(new String[]{line,operation, line1}));
     }
 
 }
