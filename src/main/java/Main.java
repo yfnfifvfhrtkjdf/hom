3c11d2c0cdafd97cc28a483fc7cd597f82678ae7
123 <rotomop11@yandex.ru>
2022-03-10 13:34:16 +0300

    3 dz

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,42 @@
 
 import model.Calculator;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-public class Main {
-
+public class Main extends Calculator {
+  static Scanner scanner = new Scanner(System.in);
 
   public static void main(String[] args) {
+    String val1 = "";
+    String val2 = "";
+    String operation = null;
+    Calculator calc = new Calculator();
+    System.out.println(val1= scanner.nextLine());
+    System.out.println( val2=scanner.nextLine());
+    System.out.println( operation=scanner.nextLine());
+    calc.execute(new String[]{val1, operation, val2});
+
+
+
+
+
+
+    // public static void main(String[] args) {
     //  считать из консоли три строки (оператор и два числа),
     //  затем вызвать на этих данных метод execute класса
     //  Calculator и вывести результат обратно в консоль.
-    System.out.println("Первое число :");
+   /* System.out.println("Первое число :");
     Scanner in = new Scanner(System.in);
-    String line = in.nextLine();
+    String line = in.nextLine(Calculator.execute(new String[]{}));
     System.out.println("операция :");
     String line2 = in.nextLine();
     System.out.println("Второе число :");
     String line3 = in.nextLine();
     System.out.println();
-    System.out.println("Результат:"+ "1: %s 2 %s 3 %s"+ Calculator.execute(new String[]{String.valueOf(line), String.valueOf(line2), String.valueOf(line3)}));
-
+    System.out.println("Результат:" + Calculator.execute(new String[]{String.valueOf(line), String.valueOf(line2), String.valueOf(line3)}));
+*/
   }
-  }
+}
 
 
 
