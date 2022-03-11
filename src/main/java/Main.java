f6a8336b73ee7bd6077843a2813b68c911175495
123 <rotomop11@yandex.ru>
2022-03-11 06:51:16 +0300

    3 dz

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,14 +6,18 @@ public class Main  {
   public static void main(String[] args) {
     Calculator calculator = new Calculator();
     Scanner in = new Scanner(System.in);
-    //System.out.println("операция :");
+    System.out.println("операция :");
     String line2 = in.nextLine();
-  //  System.out.println("Введите первое число ");
+    System.out.println(line2);
+    System.out.println("Введите первое число ");
     String line = in.nextLine();
-   // System.out.println("Введите второе число :");
+    System.out.println(line);
+    System.out.println("Введите второе число :");
     String line3 = in.nextLine();
+    System.out.println(line3);
    // String s = execute(new String[]{line2, line,line3 });
    // System.out.println(s);
+    System.out.println(line2+" "+line+" "+line3);
     System.out.println("Результат:" + execute(new String[]{line2, line,line3 }));
 
 }
