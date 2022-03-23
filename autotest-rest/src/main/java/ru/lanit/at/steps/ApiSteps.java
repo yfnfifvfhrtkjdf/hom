3614870bc393577535d37efc3e398d684afe8b21
123 <rotomop11@yandex.ru>
2022-03-23 14:23:42 +0300

    новая ветка с шаблоном

--- autotest-rest/src/main/java/ru/lanit/at/steps/ApiSteps.java
+++ autotest-rest/src/main/java/ru/lanit/at/steps/ApiSteps.java
@@ -102,28 +102,7 @@ public class ApiSteps {
             LOG.info("Сравнение значений: {} {} {}", expect, it.get(1), actual);
         });
     }
-    @И("замена контекстных переменных на значения")
-    public static String replaceVars(String preBody, Map<String, Object> vars) {
-        StringBuilder replacedText = new StringBuilder(preBody);
 
-        final String patternStartVar = "${";
-        final String patternEndVar = "}";
-
-        while (true) {
-            int fi = replacedText.indexOf(patternStartVar);
-            if (fi == -1) {
-                break;
-            }
-            int li = replacedText.indexOf(patternEndVar, fi);
-            String var = replacedText.substring(fi + patternStartVar.length(), li);
-            if (vars.containsKey(var)) {
-                replacedText.replace(fi, li + 1, vars.get(var).toString());
-            } else {
-                break;
-            }
-        }
-        return replacedText.toString();
-    }
 
     @И("подождать {int} сек")
     public void waitSeconds(int timeout) {
